57346361641445fc3a895ec65cedaa6267fe2247
Rodrigo <rodrigoperezd@gmail.com>
2020-05-19 12:33:16 +0200

    Modified gym environments added

---
+++ gym/envs/classic_control/__init__.py
@@ -0,0 +1,8 @@
+from gym.envs.classic_control.cartpole import CartPoleEnv
+from gym.envs.classic_control.mountain_car import MountainCarEnv
+from gym.envs.classic_control.continuous_mountain_car import Continuous_MountainCarEnv
+from gym.envs.classic_control.continuous_mountain_car_HD import Continuous_MountainCarHDEnv
+from gym.envs.classic_control.pendulum import PendulumEnv
+from gym.envs.classic_control.pendulum_HD import PendulumEnvHD
+from gym.envs.classic_control.acrobot import AcrobotEnv
+
